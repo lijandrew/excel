a1e3fa4a8f3dd34678a85b12ced11a10292c2512
Andrew <lijjandrew@gmail.com>
2020-08-07 16:37:07 -0400

    beginning scroll anis

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
-let burger = document.querySelector("#burger");
-let burgerBars = burger.querySelectorAll("#burger > div");
-let mobileNav = document.querySelector("nav#mobile-nav");
 
-let burgerTl = gsap.timeline();
-burgerTl.pause();
-burgerTl.to(burgerBars, {background: "white", duration: 0.1}, "b1")
-        .to(burgerBars[0], {transform: "translate(0, 100%)", duration: 0.1}, "b1")
-        .to(burgerBars[1], {margin: 0, opacity: 0, duration: 0.1}, "b1")
-        .to(burgerBars[2], {transform: "translate(0, -100%)", duration: 0.1}, "b1")
-        .to(burgerBars[0], {transform: "translate(0, 100%) rotate(45deg)", duration: 0.1}, "b2")
-        .to(burgerBars[2], {transform: "translate(0, -100%) rotate(-45deg)", duration: 0.1}, "b2");
+function setupMobileAni() {
+  "use strict";
+  let burger = document.querySelector("#burger");
+  let burgerBars = burger.querySelectorAll("#burger > div");
+  let mobileNav = document.querySelector("nav#mobile-nav");
 
-let openMobileNavTl = gsap.timeline();
-openMobileNavTl.pause();
-openMobileNavTl.set(mobileNav, {display: "block"})
-               .to("#mobile-nav-bg", {opacity: 1, duration: 0.3}, "mnavIn")
-               .to("#mobile-nav a span", {opacity: 1, transform: "translate(0, 0)", duration: 0.3, stagger: {amount: 0.3}}, "mnavIn");
+  let burgerTl = gsap.timeline();
+  burgerTl.pause();
+  burgerTl.to(burgerBars, {background: "white", duration: 0.1}, "b1")
+          .to(burgerBars[0], {transform: "translate(0, 100%)", duration: 0.1}, "b1")
+          .to(burgerBars[1], {margin: 0, opacity: 0, duration: 0.1}, "b1")
+          .to(burgerBars[2], {transform: "translate(0, -100%)", duration: 0.1}, "b1")
+          .to(burgerBars[0], {transform: "translate(0, 100%) rotate(45deg)", duration: 0.1}, "b2")
+          .to(burgerBars[2], {transform: "translate(0, -100%) rotate(-45deg)", duration: 0.1}, "b2");
 
+  let openMobileNavTl = gsap.timeline();
+  openMobileNavTl.pause();
+  openMobileNavTl.set(mobileNav, {display: "block"})
+                 .to("#mobile-nav-bg", {opacity: 1, duration: 0.3}, "mnavIn")
+                 .to("#mobile-nav a span", {opacity: 1, transform: "translate(0, 0)", duration: 0.3, stagger: {amount: 0.3}}, "mnavIn");
 
-let closeMobileNavTl = gsap.timeline();
-closeMobileNavTl.pause();
-closeMobileNavTl.to("#mobile-nav-bg", {opacity: 0, duration: 0.2}, "mnavOut")
-                .to("#mobile-nav a span", {opacity: 0, transform: "translate(0, 100%)", duration: 0.2}, "mnavOut")
-                .set(mobileNav, {display: "none"});
+  let closeMobileNavTl = gsap.timeline();
+  closeMobileNavTl.pause();
+  closeMobileNavTl.to("#mobile-nav-bg", {opacity: 0, duration: 0.2}, "mnavOut")
+                  .to("#mobile-nav a span", {opacity: 0, transform: "translate(0, 100%)", duration: 0.2}, "mnavOut")
+                  .set(mobileNav, {display: "none"});
 
-burger.addEventListener("click", () => {
-  mobileNav.classList.toggle("open");
-  if (mobileNav.classList.contains("open")) {
-    burgerTl.restart();
-    closeMobileNavTl.pause();
-    openMobileNavTl.restart();
-  } else {
-    burgerTl.reverse();
-    openMobileNavTl.pause();
-    closeMobileNavTl.restart();
-  }
-});
-
-let mobileNavLinks = document.querySelectorAll("#mobile-nav-links > a");
-for (let link of Array.from(mobileNavLinks)) {
-  link.addEventListener("click", () => {
-    mobileNav.classList.remove("open");
-    burgerTl.reverse();
-    openMobileNavTl.pause();
-    closeMobileNavTl.restart();
+  burger.addEventListener("click", () => {
+    mobileNav.classList.toggle("open");
+    if (mobileNav.classList.contains("open")) {
+      burgerTl.restart();
+      closeMobileNavTl.pause();
+      openMobileNavTl.restart();
+    } else {
+      burgerTl.reverse();
+      openMobileNavTl.pause();
+      closeMobileNavTl.restart();
+    }
   });
+
+  let mobileNavLinks = document.querySelectorAll("#mobile-nav-links > a");
+  for (let link of Array.from(mobileNavLinks)) {
+    link.addEventListener("click", () => {
+      mobileNav.classList.remove("open");
+      burgerTl.reverse();
+      openMobileNavTl.pause();
+      closeMobileNavTl.restart();
+    });
+  }
 }
+
+function setupScrollAni() {
+  gsap.to("#phone-svg", {opacity: 1, transform: "translate(0, -10%)", delay: 0.3, duration: 1, ease: "power1.out"});
+  gsap.to("#reading-svg", {opacity: 1, transform: "translate(10%, 0)", delay: 0.3, duration: 1, ease: "power1.out"});
+}
+
+function main() {
+  setupMobileAni();
+  setupScrollAni();
+}
+
+main();
